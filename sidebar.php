d42480f367b9d871dafbc3bad85d5401097d5c1f
Puneet Sahalot <puneetsahalot@gmail.com>
2014-03-10 22:25:15 +0530

    removed multipled sidebars. Added a new sidebar for EDD pages

--- sidebar.php
+++ sidebar.php
@@ -6,27 +6,21 @@
  * @since Tatva 1.0
  */
 ?>
-<?php if(is_active_sidebar('sidebar-main') || is_active_sidebar( 'sidebar-blog' ) || is_active_sidebar( 'sidebar-single' ) || is_active_sidebar( 'sidebar-page' )) { ?>
+<?php if(is_active_sidebar('sidebar-main')) { ?>
 	<div class="col grid_4_of_12">
 
 		<div id="secondary" class="sidebar" role="complementary">
 			<?php
 			do_action( 'before_sidebar' );
+                        
+                        if (is_singular('download') || is_post_type_archive('download')) {
+                            if (is_active_sidebar('sidebar-shop')) {
+                                dynamic_sidebar('sidebar-shop');  
+                            }
+                        }
 
 			if ( is_active_sidebar( 'sidebar-main' ) ) {
 				dynamic_sidebar( 'sidebar-main' );
-			}
-
-			if ( ( is_home() || is_archive() ) && is_active_sidebar( 'sidebar-blog' ) ) {
-				dynamic_sidebar( 'sidebar-blog' );
-			}
-
-			if ( is_single() && is_active_sidebar( 'sidebar-single' ) ) {
-				dynamic_sidebar( 'sidebar-single' );
-			}
-
-			if ( is_page() && is_active_sidebar( 'sidebar-page' ) ) {
-				dynamic_sidebar( 'sidebar-page' );
 			}
 			?>
 
